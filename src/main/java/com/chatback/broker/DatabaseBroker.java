e63d862e2b249e62212651a31671a560d33c702d
chadforgit <chadforgit@gmail.com>
2018-06-26 08:10:47 -0700

    spring

--- src/main/java/com/chatback/broker/DatabaseBroker.java
+++ src/main/java/com/chatback/broker/DatabaseBroker.java
@@ -1,10 +1,47 @@
 package com.chatback.broker;
 
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Logger;
+
+import static java.sql.DriverManager.getConnection;
+
 public class DatabaseBroker
 {
 
     public void getMessages(String id)
     {
-        //Call DB
+        // This SQL statement produces all table
+        // names and column names in the H2 schema
+        String sql = "select table_name, column_name " +
+                "from information_schema.columns " +
+                "order by " +
+                "table_catalog, " +
+                "table_schema, " +
+                "table_name, " +
+                "ordinal_position";
+
+        connectToDatabase(sql);
+
+
+    }
+
+    private void connectToDatabase(String sql) {
+        try (Connection c = getConnection(
+                "localhost:3307",
+                "root", "installedSQL9")) {
+
+            Statement statement = c.createStatement();
+
+            ResultSet set = statement.executeQuery(sql);
+
+
+        }
+        catch (Exception e)
+        {
+            Logger.getAnonymousLogger().info(e.getLocalizedMessage());
+        }
     }
 }
